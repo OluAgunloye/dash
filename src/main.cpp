dcfd32039746ff6e7fbbd347c3564160ab8b5590
heliumpay <heliumpay@gmail.com>
2017-07-26 15:26:04 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -116,7 +116,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "DarkCoin Signed Message:\n";
+const string strMessageMagic = "Helium Signed Message:\n";
 
 // Internal stuff
 namespace {
