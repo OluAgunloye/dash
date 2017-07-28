f0f1e3157b1ffb94502b66df6e66d2cda7c0b2b1
heliumpay <heliumpay@gmail.com>
2017-07-28 14:11:23 +0100

    Update darksend.cpp

--- src/darksend.cpp
+++ src/darksend.cpp
@@ -433,8 +433,8 @@ void CDarksendPool::InitDenominations()
         is convertable to another.
 
         For example:
-        1DRK+1000 == (.1DRK+100)*10
-        10DRK+10000 == (1DRK+1000)*10
+        1HLM+1000 == (.1HLM+100)*10
+        10HLM+10000 == (1HLM+1000)*10
     */
     /* Disabled
     vecPrivateSendDenominations.push_back( (100      * COIN)+100000 );
